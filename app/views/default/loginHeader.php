7b3135cbb88621b81f37fd8750587292bdf45e52
denisNiko <denisnikolai4uk@gmail.com>
2024-11-11 14:10:05 +0200

    updated index

---
+++ app/views/default/loginHeader.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Todo List</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!--<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>-->
+    
+</head>
+<body class="bg-light">
